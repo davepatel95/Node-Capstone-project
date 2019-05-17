6b36cda56c5b111f0c77157600b1b1556dfa8a86
Dave Patel <davepatel95@gmail.com>
2019-05-17 16:42:21 -0700

    added test

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
 'use strict';
 
-const isEqual = require('../isEqual');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-describe('isEqual', function() {
-    it('should give right answers for equal and unequal inputs');
+
+const expect = require('chai').expect;
+const express = require('express');
+const app = express();
+
+chai.use(chaiHttp);
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
+
+describe('/', function() {
+    it('should return status of 200', function () {
+        let res;
+        return chai.request(app)
+            .get('/')
+            .then(function (_res){
+                res = _res;
+                expect(res).to.have.status(200);
+            })
+    });
   }
 );
